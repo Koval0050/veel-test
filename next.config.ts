be9ccf916b4f604a3e583232a6b44072e5337e1f
Koval Vasyl <121100028+Koval0050@users.noreply.github.com>
2025-02-09 23:51:51 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   basePath: "/veel-test",
-  assetPrefix: "/veel-test",
+  images: {
+    unoptimized: true,
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
